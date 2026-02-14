e645b5b8a64ec7d9f93bfacad3c96af8c7c36612
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-02-14 16:25:08 +0545

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
